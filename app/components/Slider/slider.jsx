dccdc0b79b8a13ad9ac18bf1c157794a83db6bf1
Zohrab Samadzade <zohrabsemedzade3@gmail.com>
2024-07-06 00:41:46 +0400

    module css imported to the slider file

--- app/components/Slider/slider.jsx
+++ app/components/Slider/slider.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import Swiper from 'swiper';
 import 'swiper/css';
+import styles from './slider.module.css';
 
 export default function Slider() {
     const swiper = new Swiper('.swiper', {
